ad1ab49229b5991ece2e21edd4d5766db5a08c73
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-07-29 17:39:35 +0530

    Add files via upload

---
+++ TERRAFORM/modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+type = string
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_app_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_db_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
