5b8a52b9372875c82fa4ccf26d2fec57148120b9
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-07-29 17:31:22 +0530

    Add files via upload

---
+++ TERRAFORM/variables.tf
@@ -0,0 +1,100 @@
+
+variable "vpc_cidr" {
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+}
+
+variable "private_app_subnet_cidrs" {
+  type        = list(string)
+}
+
+variable "private_db_subnet_cidrs" {
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  type        = list(string)
+}
+
+# SG
+
+variable "security_group_names" {
+  type = list(string)
+}
+
+variable "alb_ports" {
+  type = list(number)
+}
+
+variable "app_ports" {
+  type = list(number)
+}
+
+variable "db_ports" {
+  type = list(number)
+}
+
+variable "public_ports" {
+  type = list(number)
+}  
+
+
+# RDS
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_engine" {
+  type = string
+}
+
+variable "db_engine_version" {
+  type = string
+}
+
+variable "db_instance_class" {
+  type = string
+}
+
+variable "allocated_storage" {
+  type = number
+}
+
+
+# ALB
+
+variable "alb_name" {
+  type = string
+}
+
+variable "target_group_name" {
+  type = string
+}
+
+
+# EC2_Instnace
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
