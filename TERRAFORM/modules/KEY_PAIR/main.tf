9e8fcc4816d0f0a3df885c6aafa05a9f0a3e3514
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-08-04 12:47:46 +0530

    Add files via upload

---
+++ TERRAFORM/modules/KEY_PAIR/main.tf
@@ -0,0 +1,15 @@
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  filename        = "/mnt/c/Users/PAYAL KHARAT/Downloads/${var.key_name}.pem"
+  content         = tls_private_key.ssh_key.private_key_pem
+  file_permission = "0400"
+}
