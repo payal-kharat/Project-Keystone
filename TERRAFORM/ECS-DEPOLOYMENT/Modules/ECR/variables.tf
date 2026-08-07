9ea643e50c5e5b54b26ff92d7aea9f809992e872
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-08-07 11:26:29 +0530

    Add files via upload

---
+++ TERRAFORM/ECS-DEPOLOYMENT/Modules/ECR/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+
+  description = "Project Name"
+
+  type = string
+
+}
+
+variable "frontend_repository_name" {
+
+  description = "Frontend ECR Repository"
+
+  type = string
+
+}
+
+variable "backend_repository_name" {
+
+  description = "Backend ECR Repository"
+
+  type = string
+
+}
