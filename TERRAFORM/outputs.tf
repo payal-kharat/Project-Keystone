280367b88eb3ed755f6a040703d88b410934e34d
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-08-07 16:34:34 +0530

    Update outputs.tf

--- TERRAFORM/outputs.tf
+++ TERRAFORM/outputs.tf
@@ -1,29 +1,21 @@
 output "rds_endpoint" {
   value = module.rds.rds_endpoint
 }
-
 output "database_name" {
   value = module.rds.database_name
 }
-
 output "database_port" {
   value = module.rds.database_port
 }
-
-
 output "alb_dns_name" {
   value = module.alb.alb_dns_name
 }
-
 output "target_group_arn" {
   value = module.alb.target_group_arn
 }
-
-
 output "instance_id" {
   value = module.ec2.instance_id
 }
-
 output "private_ip" {
   value = module.ec2.private_ip
 }
