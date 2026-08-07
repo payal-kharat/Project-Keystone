fa6c5bc108f40a26184dece34536570b97f51f55
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-08-07 11:28:06 +0530

    Add files via upload

---
+++ TERRAFORM/ECS-DEPOLOYMENT/Modules/ECS-TASK/outputs.tf
@@ -0,0 +1,17 @@
+output "task_definition_arn" {
+
+  value = aws_ecs_task_definition.this.arn
+
+}
+
+output "task_definition_family" {
+
+  value = aws_ecs_task_definition.this.family
+
+}
+
+output "task_definition_revision" {
+
+  value = aws_ecs_task_definition.this.revision
+
+}
