2bcd833dedb4c51eb000f3b24cad6386e6a70166
payal-kharat <160392242+payal-kharat@users.noreply.github.com>
2026-08-10 22:27:05 +0530

    Update variables.tf

--- TERRAFORM/ECS-DEPOLOYMENT/variables.tf
+++ TERRAFORM/ECS-DEPOLOYMENT/variables.tf
@@ -1,59 +1,42 @@
 variable "AWS_REGION" {
   type = string
 }
-
 variable "PROJECT_NAME" {
   type = string
 }
-
 variable "VPC_CIDR" {
   type = string
 }
-
 variable "PUBLIC_SUBNETS" {
   type = list(string)
 }
-
 variable "PRIVATE_APP_SUBNETS" {
   type = list(string)
 }
-
 variable "PRIVATE_DB_SUBNETS" {
   type = list(string)
 }
-
 variable "AVAILABILITY_ZONES" {
   type = list(string)
 }
 
 # SG
-
-variable "ALB_INGRESS_PORT" {
-  type = number
-}
-
-variable "ALB_INGRESS_PROTOCOL" {
-  type = string
-}
-
-variable "ALB_INGRESS_CIDR" {
-  type = list(string)
-}
-
-variable "ECS_PORT" {
-  type = number
-}
-
-variable "ECS_PROTOCOL" {
-  type = string
-}
-
-variable "DB_PORT" {
-  type = number
-}
-
-variable "DB_PROTOCOL" {
-  type = string
+variable "SECURITY_GROUPS" {
+  type = map(object({
+    name = string
+    ingress_rules = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+    egress_rules = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }))
+  }))
 }
 
 # ECR
@@ -61,7 +44,6 @@ variable "DB_PROTOCOL" {
 variable "FRONTEND_REPOSITORY_NAME" {
   type = string
 }
-
 variable "BACKEND_REPOSITORY_NAME" {
   type = string
 }
@@ -71,27 +53,21 @@ variable "BACKEND_REPOSITORY_NAME" {
 variable "ALB_NAME" {
   type = string
 }
-
 variable "TARGET_GROUP_NAME" {
   type = string
 }
-
 variable "LISTENER_PORT" {
   type = number
 }
-
 variable "LISTENER_PROTOCOL" {
   type = string
 }
-
 variable "TARGET_GROUP_PORT" {
   type = number
 }
-
 variable "TARGET_GROUP_PROTOCOL" {
   type = string
 }
-
 variable "HEALTH_CHECK_PATH" {
   type = string
 }
@@ -111,16 +87,13 @@ variable "DB_IDENTIFIER" {
 variable "DB_NAME" {
   type = string
 }
-
 variable "DB_USERNAME" {
   type = string
 }
-
 variable "DB_PASSWORD" {
   type      = string
   sensitive = true
 }
-
 variable "DB_INSTANCE_CLASS" {
   type = string
 }
@@ -138,19 +111,15 @@ variable "ALLOCATED_STORAGE" {
 variable "TASK_FAMILY" {
   type = string
 }
-
 variable "CPU" {
   type = number
 }
-
 variable "MEMORY" {
   type = number
 }
-
 variable "FRONTEND_IMAGE" {
   type = string
 }
-
 variable "BACKEND_IMAGE" {
   type = string
 }
@@ -160,11 +129,9 @@ variable "BACKEND_IMAGE" {
 variable "SERVICE_NAME" {
   type = string
 }
-
 variable "DESIRED_COUNT" {
   type = number
 }
-
 variable "FRONTEND_CONTAINER_NAME" {
   type = string
 }
